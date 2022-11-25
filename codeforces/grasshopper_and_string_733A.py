5da03333536eecdd1faaa7c27e7807b7dd1e7cef
Raffian-moin <raffianmoin69@gmail.com>
2022-11-26 00:46:55 +0600

    grasshopper and string

---
+++ codeforces/grasshopper_and_string_733A.py
@@ -0,0 +1,13 @@
+vowels = ["A", "E", "I", "O", "U","Y"]
+
+s = input()
+c = 1
+jump = 1
+for ch in s:
+    if ch not in vowels:
+        c = c+1
+        jump = max(c, jump)
+    else:
+        c = 1
+
+print(jump)
