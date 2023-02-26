a404eb4d92362004ee39b07d5ae6647641690549
Raffian-moin <raffianmoin69@gmail.com>
2023-02-26 13:44:07 +0600

    accepted

---
+++ codeforces/1000 problems/string/1473_string_LCM.py
@@ -0,0 +1,20 @@
+import math
+for _ in range(int(input())):
+    s = input()
+    t = input()
+
+    if len(s)*t != len(t)*s:
+        print(-1)
+    else:
+        print((math.lcm(len(s), len(t))//len(s))*s)
+
+
+# === another solution using gcd ===
+
+# import math
+# for _ in range(int(input())):
+#     s = input()
+#     t = input()
+#     q = math.gcd(len(s), len(t))
+#     ss,tt = s * (len(t)//q),t*(len(s)//q)
+#     print(ss if ss==tt else -1)
