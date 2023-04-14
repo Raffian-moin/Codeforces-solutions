7267082d09c97ca8f68d6b5a0133ac749f4cd0b8
Raffian-moin <raffianmoin69@gmail.com>
2023-04-14 15:09:38 +0600

    accepted, okk

--- codeforces/koxia_and_permutation.py
+++ codeforces/koxia_and_permutation.py
@@ -1,26 +1,17 @@
-n, m = input().split()
-c = int(n)-int(m)+1
-arr = []
-for i in range(1, int(n)+1):
-    arr.append(i)
-
-new_arr = []
-last = len(arr)-1
-first = 0
-flag = 0
-for i in range(len(arr)):
-    if flag ==1:
-        break
-    new_arr.append(arr[last])
-    last = last-1
-    
-    for j in range(c-1):
-        # print(last)
-        if last==first:
-            flag = 1
-            break
+for _ in range(int(input())):
+    n, m = list(map(int, input().split()))
+    r=0
+    l=1
+    for i in range(1, n+1):
+        if i%2==1:
+            print(n-r, end=" ")
+            r+=1
         else:
-            new_arr.append(arr[first])
-            first=first+1
+            print(l, end=" ")
+            l+=1
+    print()
 
-print(new_arr)
+# can also be written as 
+# for i in range(n//2):
+# print(n-i, i+1, end=' ')
+# print(n//2+1 if n & 1 else '')
