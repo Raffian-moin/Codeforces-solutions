02548b85be0bf717d90b77eeb1297e7a6309b306
Raffian-moin <raffianmoin69@gmail.com>
2023-06-21 17:15:17 +0600

    change folder name

--- codeforces/800 Problems/coins.py
+++
@@ -1,7 +0,0 @@
-for _ in range(int(input())):
-    n, k=list(map(int, input().split()))
-
-    if n%2==k%2 or n%2==0:
-        print('YES')
-    else:
-        print('NO')
--- codeforces/800 Problems/karina_and_array.py
+++
@@ -1,8 +0,0 @@
-for _ in range(int(input())):
-    n=int(input())
-    a=list(map(int, input().split()))
-    a.sort()
-    print(a[n-1]*a[n-2] if a[n-1]*a[n-2]> a[0]*a[1] else a[0]*a[1])
-
-    # can also be written as
-    # print(max(a[0] * a[1], a[-1] * a[-2]))
